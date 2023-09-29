1fb4e5c4c25c76ab29db3e8fd6909949fcb5ce91
Sean Kipinä <paintballsean@gmail.com>
2023-09-29 23:13:53 +0300

    Task 4 completed index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,40 +1,49 @@
 async function moduleProject4() {
-
   // 👇 WORK WORK BELOW THIS LINE 👇
-  const footer = document.querySelector('footer')
-  const currentYear = new Date().getFullYear()
-  footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
+  const footer = document.querySelector("footer");
+  const currentYear = new Date().getFullYear();
+  footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`;
 
-
-  
   let descriptions = [
     ["Sunny", "☀️"],
     ["Cloudy", "☁️"],
     ["Rainy", "🌧️"],
     ["Thunderstorm", "⛈️"],
     ["Snowy", "❄️"],
-    ["Partly Cloudy", "⛅️"]
-  ]
+    ["Partly Cloudy", "⛅️"],
+  ];
 
   // 👉 Tasks 1 - 5 go here
-  document.querySelector('#weatherWidget').style.display = 'none';
-  document.querySelector('#citySelect').addEventListener('change', async (evt) => {
-    console.log('selection changed');
-    try {
-      document.querySelector('#citySelect').setAttribute('disabled', 'disabled');
-      document.querySelector('#weatherWidget').style.display = 'none';
-      document.querySelector('.info').textContent = 'Fetching weather data...';
-    } catch (err) {
-      // Handle errors here
-      console.error(err);
-    }
-  });
-  // 👆 WORK WORK ABOVE THIS LINE 👆
+  document.querySelector("#weatherWidget").style.display = "none";
+  document
+    .querySelector("#citySelect")
+    .addEventListener("change", async (evt) => {
+      console.log("selection changed");
+      try {
+        document
+          .querySelector("#citySelect")
+          .setAttribute("disabled", "disabled");
+        document.querySelector("#weatherWidget").style.display = "none";
+        document.querySelector(".info").textContent =
+          "Fetching weather data...";
 
+        console.log(evt.target.value);
+        let city = evt.target.value;
+        let url = `http://localhost:3003/api/weather?city=${city}`;
+        console.log(url);
+
+        const res = await axios.get(url);
+        console.log(res.data);
+      } catch (err) {
+        console.log('The promise was rejected with an err.message --> ', err.message)
+      }
+    });
+  // 👆 WORK WORK ABOVE THIS LINE 👆
 }
 
 // ❗ DO NOT CHANGE THE CODE  BELOW
 // ❗ DO NOT CHANGE THE CODE  BELOW
 // ❗ DO NOT CHANGE THE CODE  BELOW
-if (typeof module !== 'undefined' && module.exports) module.exports = { moduleProject4 }
-else moduleProject4()
+if (typeof module !== "undefined" && module.exports)
+  module.exports = { moduleProject4 };
+else moduleProject4();
